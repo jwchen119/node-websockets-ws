308ff5ec25ef680840e42395d790fb94915807e9
jwchen119 <jwchen119@gmail.com>
2018-02-21 12:08:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,16 +7,29 @@ const bfx = new BFX({
     apiSecret: '4321',
 })
 
-const SYMBOL = 'tIOTEUR'
-const ws = bfx.ws(2)
-
-ws.on('open', () => {
-  console.log('open')
-  ws.subscribeTicker(SYMBOL)
+const ws = bfx.ws(2, {
+  manageCandles: true,  // enable candle dataset persistence/management
+  transform: true       // converts ws data arrays to Candle models (and others)
 })
 
-ws.onTicker({ symbol: SYMBOL }, (ticker) => {
-  console.log('%s ticker: %j', SYMBOL, ticker)
+const CANDLE_KEY = 'trade:1m:tBTCUSD'
+
+ws.on('open', () => {
+  debug('open')
+  ws.subscribeCandles(CANDLE_KEY)
+})
+
+ws.onCandle({ key: CANDLE_KEY }, (candles) => {
+  if (prevTS === null || candles[0].mts > prevTS) {
+    const c = candles[1] // report previous candle
+
+    debug(`%s %s open: %f, high: %f, low: %f, close: %f, volume: %f`,
+      CANDLE_KEY, new Date(c.mts).toLocaleTimeString(),
+      c.open, c.high, c.low, c.close, c.volume
+    )
+
+    prevTS = candles[0].mts
+  }
 })
 
 ws.open()
